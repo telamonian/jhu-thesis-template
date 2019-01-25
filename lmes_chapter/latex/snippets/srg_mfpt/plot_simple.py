b3b2c329e3bca7c5d9cf5588edc42cedd5cb8524
telamonian <mklein@jhu.edu>
2019-01-24 23:32:55 -0500

    added note clarifying note about landscape
    clarified the difference between the steady state probability distribution and the epigenetic landscape

--- lmes_chapter/latex/snippets/srg_mfpt/plot_simple.py
+++ lmes_chapter/latex/snippets/srg_mfpt/plot_simple.py
@@ -6,6 +6,6 @@ ax = fig.add_axes([0, 0, 1, 1])
 for summary in summaries:
     ax.plot(summary.edges, summary.first_passage_times)
 
-ax.set_xlabel(r'$𝚨$')
+ax.set_xlabel('$\N{MATHEMATICAL BOLD CAPITAL ALPHA}$')
 ax.set_ylabel('time')
 pass
